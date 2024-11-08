5af154c8aed25019c4d55302ca21b64ea1df0b91
Emerson Vieira <emerson.chaves@nuvem.net>
2024-11-08 19:07:00 -0400

    chore: add tailwind prettier plugin

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="items-center justify-center flex w-screen">
+      <h1 className="text-red-500">Hello World</h1>
     </div>
   );
 }
