591c69580c6e3c3f2cacba3b4dfef7c96b1af9e3
Emerson Vieira <emerson.chaves@nuvem.net>
2024-11-09 16:49:16 -0400

    feat: ensure transaction table labels are rendered property

--- app/transactions/_columns/index.tsx
+++ app/transactions/_columns/index.tsx
@@ -3,6 +3,12 @@
 import { Transaction } from "@prisma/client";
 import { ColumnDef } from "@tanstack/react-table";
 import { TypeBadge } from "../_components/type-badge";
+import {
+  TRANSACTION_CATEGORY_LABELS,
+  TRANSACTION_PAYMENT_METHOD_LABELS,
+} from "../_enums";
+import { Button } from "@/app/_components/ui/button";
+import { PencilIcon, TrashIcon } from "lucide-react";
 
 export const transactionColumns: ColumnDef<Transaction>[] = [
   {
@@ -19,21 +25,46 @@ export const transactionColumns: ColumnDef<Transaction>[] = [
   {
     accessorKey: "category",
     header: "Categoria",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_CATEGORY_LABELS[transaction.category],
   },
   {
     accessorKey: "paymentMethod",
     header: "Metodo de pagamento",
+    cell: ({ row: { original: transaction } }) =>
+      TRANSACTION_PAYMENT_METHOD_LABELS[transaction.paymentMethod],
   },
   {
     accessorKey: "date",
     header: "Data",
+    cell: ({ row: { original: transaction } }) =>
+      new Date(transaction.date).toLocaleDateString("pt-BR", {
+        day: "2-digit",
+        month: "long",
+        year: "numeric",
+      }),
   },
   {
     accessorKey: "amount",
     header: "Valor",
+    cell: ({ row: { original: transaction } }) =>
+      new Intl.NumberFormat("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+      }).format(Number(transaction.amount)),
   },
   {
     accessorKey: "actions",
     header: "",
+    cell: () => (
+      <div className="flex gap-2">
+        <Button variant="ghost" size="icon" className="text-muted-foreground">
+          <PencilIcon />
+        </Button>
+        <Button variant="ghost" size="icon" className="text-muted-foreground">
+          <TrashIcon />
+        </Button>
+      </div>
+    ),
   },
 ];
---
+++ app/transactions/_enums/index.ts
@@ -0,0 +1,21 @@
+export enum TRANSACTION_CATEGORY_LABELS {
+  EDUCATION = "Educação",
+  ENTERTAINMENT = "Entreterimento",
+  FOOD = "Alimentação",
+  HEALTH = "Saúde",
+  HOUSING = "Moradia",
+  OTHER = "Outros",
+  SALARY = "Salário",
+  TRANSPORTATION = "Transporte",
+  UTILITY = "Utilidades",
+}
+
+export enum TRANSACTION_PAYMENT_METHOD_LABELS {
+  BANK_TRANSFER = "Transferência Bancária",
+  BANK_SLIP = "Boleto Bancário",
+  CASH = "Dinheiro",
+  CREDIT_CARD = "Cartão de crédito",
+  DEBIT_CARD = "Cartão de débito",
+  OTHER = "Outros",
+  PIX = "Pix",
+}
