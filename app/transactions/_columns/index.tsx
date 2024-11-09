ae09fb4ae96d784857e86d3b96e22f915a6256f8
Emerson Vieira <emerson.chaves@nuvem.net>
2024-11-09 16:16:16 -0400

    feat: add transaction table and column type badge

---
+++ app/transactions/_columns/index.tsx
@@ -0,0 +1,39 @@
+"use client";
+
+import { Transaction } from "@prisma/client";
+import { ColumnDef } from "@tanstack/react-table";
+import { TypeBadge } from "../_components/type-badge";
+
+export const transactionColumns: ColumnDef<Transaction>[] = [
+  {
+    accessorKey: "name",
+    header: "Nome",
+  },
+  {
+    accessorKey: "type",
+    header: "Tipo",
+    cell: ({ row: { original: transaction } }) => (
+      <TypeBadge transaction={transaction} />
+    ),
+  },
+  {
+    accessorKey: "category",
+    header: "Categoria",
+  },
+  {
+    accessorKey: "paymentMethod",
+    header: "Metodo de pagamento",
+  },
+  {
+    accessorKey: "date",
+    header: "Data",
+  },
+  {
+    accessorKey: "amount",
+    header: "Valor",
+  },
+  {
+    accessorKey: "actions",
+    header: "",
+  },
+];
--- app/transactions/page.tsx
+++ app/transactions/page.tsx
@@ -1,7 +1,23 @@
-export default function TransactionsPage() {
+import { ArrowDownUp } from "lucide-react";
+import { Button } from "../_components/ui/button";
+import { db } from "../_lib/prisma";
+import { DataTable } from "../_components/ui/data-table";
+import { transactionColumns } from "./_columns";
+
+export default async function TransactionsPage() {
+  const transactions = await db.transaction.findMany({});
+
   return (
-    <div>
-      <h1>Transactions page</h1>
+    <div className="space-y-6 p-6">
+      <div className="flex w-full items-center justify-between">
+        <h1 className="text-2xl font-bold">Transações</h1>
+        <Button className="rounded-full">
+          Adicionar transação
+          <ArrowDownUp className="ml-2" />
+        </Button>
+      </div>
+
+      <DataTable columns={transactionColumns} data={transactions} />
     </div>
-  )
+  );
 }
